cf1d49bef6064a9af72900cb85e16514ff6e53d9
Minseok Jung <nminseok0917@naver.com>
2026-04-21 19:58:22 +0900

    feat: 초안 완료

--- balls/js/common.js
+++ balls/js/common.js
@@ -1,20 +1,56 @@
 const $ = (query) => document.querySelector(query);
 const $$ = (query) => [...document.querySelectorAll(query)];
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const $app = $("#app");
 const $svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 $app.append($svg);
 
-let balls = [
-  {
-    x: 100,
-    y: 100,
-    radius: 10,
+let balls = [];
+for (let i = 0; i < 100; i++) {
+  const radius = random(10, 30);
+  let speedX = random(-3, 3);
+  let speedY = random(-3, 3);
+
+  while (speedX === 0 || speedY === 0) {
+    speedX = random(-3, 3);
+    speedY = random(-3, 3);
+  }
+
+  balls.push({
+    x: random(radius, window.innerWidth - radius),
+    y: random(radius, window.innerHeight - radius),
+    radius: radius,
+    speedX: speedX,
+    speedY: speedY,
     element: null,
-    speedX: 2,
-    speedY: 2,
-  },
-];
+  });
+}
+
+const $filter = document.createElementNS("http://www.w3.org/2000/svg", "filter");
+$filter.setAttribute("id", "gooey");
+
+const $filterBlur = document.createElementNS("http://www.w3.org/2000/svg", "feGaussianBlur");
+$filterBlur.setAttribute("stdDeviation", "8");
+
+const $filterColorMatrix = document.createElementNS("http://www.w3.org/2000/svg", "feColorMatrix");
+$filterColorMatrix.setAttribute("type", "matrix");
+$filterColorMatrix.setAttribute("values", "1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 18 -7");
+
+$filter.append($filterBlur, $filterColorMatrix);
+$svg.append($filter);
+
+const $g = document.createElementNS("http://www.w3.org/2000/svg", "g");
+$g.setAttribute("filter", "url(#gooey)");
+
+$svg.append($g);
+
+// const $circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+// $circle.setAttribute("cx", "100");
+// $circle.setAttribute("cy", "100");
+// $circle.setAttribute("r", "10");
+// $circle.setAttribute("fill", "red");
+// $g.append($circle);
 
 function handleResize() {
   $svg.setAttribute("width", window.innerWidth);
@@ -29,7 +65,7 @@ function animate() {
 
     if (!ball.element) {
       ball.element = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-      $svg.appendChild(ball.element);
+      $g.appendChild(ball.element);
     }
 
     ball.element.setAttribute("cx", ball.x);
